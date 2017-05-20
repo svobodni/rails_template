5a805521718e8ff8a2fccd46c61e0df0b50c21a1
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-05-21 01:17:28 +0200

    Bootstrap

--- svobodni.rb
+++ svobodni.rb
@@ -7,6 +7,12 @@ after_bundle do
   git add: "."
   git commit: %Q{ -m 'Rails generated skeleton' }
 
+  oauth
+  bootstrap
+  welcome
+end
+
+def oauth
   # AUTH
   run 'spring stop'
   gem 'omniauth-oauth2'
@@ -26,14 +32,31 @@ after_bundle do
   copy_file "templates/error.rb", "lib/oauth2/error.rb"
 
   generate('configatron:install')
-  copy_file "templates/development.rb", "config/configatron/development.rb", force: true
+  copy_file "templates/development.rb", "config/configatron/development.rb", {force: true}
 
   route "get '/auth/:provider/callback', to: 'sessions#create'"
   route "get '/sessions/destroy'"
 
   git add: "."
   git commit: %Q{ -m 'Configured for registr outh2' }
+end
 
+def bootstrap
+  gem 'jquery-rails'
+  gem 'bootstrap-sass', '~> 3.3.6'
+  gem 'font-awesome-rails'
+  run 'bundle'
+  copy_file "templates/application.js", "app/assets/javascripts/application.js"
+  copy_file "templates/bs-setup.scss", "app/assets/stylesheets/bs-setup.scss"
+  #   gsub_file 'app/assets/stylesheets/application.css', "*= require_tree .", "@import \"bootstrap-sprockets\"
+  # @import \"bootstrap\""
+  #   gsub_file '', "//= require jquery", "//= require .bootstrap-sprockets"
+
+  git add: "."
+  git commit: %Q{ -m 'Added bootstrap' }
+end
+
+def welcome
   copy_file "templates/application.html.erb", "app/views/layouts/application.html.erb"
   generate(:controller, "welcome ahoj")
   route "root to: 'welcome#ahoj'"
